151aab056296a73fd64b0c1b40a704ddc0778b80
Vinicius Dias <92399478+vinydias23@users.noreply.github.com>
2021-11-23 21:36:41 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Vinicius 1',
+                partido: 'DEUS',
+                fotos: [
+                    { url: 'vereador1.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Vinicius 2',
+                partido: 'DEUS',
+                fotos: [
+                    { url: 'vereador2.png', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Vinicius',
+                partido: 'DEUS',
+                vice: 'JESUS',
+                fotos: [
+                    { url: 'prefeito.png', legenda: 'Prefeito' },
+                    { url: 'vice-prefeito.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Vinicius 2',
+                partido: 'DEUS',
+                vice: 'JESUS',
+                fotos: [
+                    { url: 'prefeito2.png', legenda: 'Prefeito' },
+                    { url: 'vice-prefeito2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
